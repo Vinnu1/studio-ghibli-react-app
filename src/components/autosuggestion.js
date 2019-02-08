fdc066d7fb459eef4c1c2761484b117f1e9dcc3d
Vinayak Sinha <vinnusinha@gmail.com>
2019-02-09 04:52:37 +0530

    search needs to be fix;plus add more filters

---
+++ src/components/autosuggestion.js
@@ -0,0 +1,237 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Popper from '@material-ui/core/Popper';
+import { withStyles } from '@material-ui/core/styles';
+
+
+function renderInputComponent(inputProps) {
+  const { classes, inputRef = () => { }, ref, ...other } = inputProps;
+
+  return (
+    <TextField
+      fullWidth
+      className={classes.textField}
+      InputProps={{
+        inputRef: node => {
+          ref(node);
+          inputRef(node);
+        },
+        classes: {
+          input: classes.input,
+        },
+      }}
+      {...other}
+    />
+  );
+}
+
+function itemClick() {
+  console.log('a item was pressed')
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+  const matches = match(suggestion.name, query);
+  const parts = parse(suggestion.name, matches);
+
+  return (
+    <MenuItem onClick={() => { itemClick() }} selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 500 }}>
+              {part.text}
+            </span>
+          ) : (
+              <strong key={String(index)} style={{ fontWeight: 300 }}>
+                {part.text}
+              </strong>
+            );
+        })}
+      </div>
+    </MenuItem>
+  );
+}
+
+//passing actual suggestions
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+const styles = theme => ({
+  root: {
+    //height: 250,
+    flexGrow: 1,
+  },
+  input: {
+    color: '#000'
+  },
+  textField: {
+    '& label': {
+      color: theme.palette.text.secondary
+    },
+    '& input': {
+      color: theme.palette.text.secondary,
+    },
+    '& div': {
+      padding: 0
+    },
+    '& button': {
+      margin: 0
+    }
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+    '& div': {
+      color: theme.palette.text.secondary
+    }
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
+});
+
+class Autosuggestion extends React.Component {
+
+  state = {
+    single: '',
+    popper: '',
+    suggestions: [],
+  };
+
+  getSuggestions(value) {
+    const inputValue = deburr(value.trim()).toLowerCase();
+    const inputLength = inputValue.length;
+    let count = 0;
+
+    return inputLength === 0
+      ? []
+      : this.props.suggest.filter(suggestion => {
+        const keep =
+          count < 5 && suggestion.name.slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+  }
+
+  handleSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value),
+    });
+  };
+
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  handleChange = name => (event, { newValue }) => {
+    this.setState({
+      [name]: newValue,
+    });
+  };
+
+
+
+  render() {
+    const { classes } = this.props;
+    const autosuggestProps = {
+      renderInputComponent,
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+      getSuggestionValue,
+      renderSuggestion,
+    };
+
+    return (
+      <div className={classes.root}>
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            placeholder: 'Search Models by Name',
+            value: this.state.single,
+            onChange: this.handleChange('single'),
+          }}
+          theme={{
+            container: classes.container,
+            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+          }}
+          renderSuggestionsContainer={options => (
+            <Paper {...options.containerProps} square>
+              {options.children}
+            </Paper>
+          )}
+        />
+        {/* <div className={classes.divider} />
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            label: 'Label',
+            placeholder: 'With Popper',
+            value: this.state.popper,
+            onChange: this.handleChange('popper'),
+            inputRef: node => {
+              this.popperNode = node;
+            },
+            InputLabelProps: {
+              shrink: true,
+            },
+          }}
+          theme={{
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+          }}
+          renderSuggestionsContainer={options => (
+            <Popper anchorEl={this.popperNode} open={Boolean(options.children)}>
+              <Paper
+                square
+                {...options.containerProps}
+                style={{ width: this.popperNode ? this.popperNode.clientWidth : null }}
+              >
+                {options.children}
+              </Paper>
+            </Popper>
+          )}
+        /> */}
+      </div>
+    );
+  }
+}
+
+Autosuggestion.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Autosuggestion);
---
+++ src/resources/theme.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    red: '#D51E3A',
+    ghibliTheme: '#0a9bc2',
+    grey: '#E5DFE0',
+    grey1: '#7D7879',
+    white: '#FFFFFF',
+    black: '#000000'
+} 
